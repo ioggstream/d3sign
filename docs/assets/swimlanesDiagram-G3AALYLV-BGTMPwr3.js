c91ffa71477083b920f65c72ef6f5c73a47b41fd
Roberto Polli <robipolli@gmail.com>
2026-08-12 15:56:45 +0200

    Build file.

---
+++ docs/assets/swimlanesDiagram-G3AALYLV-BGTMPwr3.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-jA4cbgsh.js";import{_ as a}from"./index-DSIxmXvM.js";import"./chunk-5VM5RSS4-Y1VDyWvr.js";import"./chunk-XXDRQBXY-Bdssu7n8.js";import"./chunk-VR4S4FIN-JlT9o7f2.js";import"./chunk-32BRIVSS-BvMI8bts.js";import"./channel-f9y9DlCu.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
